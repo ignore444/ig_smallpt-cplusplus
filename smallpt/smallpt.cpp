dccdfd065bd02f0b0c1f9944aaa170784c4cbb43
ig <123@123.123>
2022-06-28 13:36:17 +0900

    - indent

--- smallpt/smallpt.cpp
+++ smallpt/smallpt.cpp
@@ -153,11 +153,17 @@ Vec radiance(const Ray& r, int depth, unsigned short* Xi)
 
 int main(int argc, char* argv[]) 
 {
-	int w = 1024, h = 768, samps = argc == 2 ? atoi(argv[1]) / 4 : 1; // # samples
-	Ray cam(Vec(50, 52, 295.6), Vec(0, -0.042612, -1).norm()); // cam pos, dir
-	Vec cx = Vec(w * .5135 / h), cy = (cx % cam.d).norm() * .5135, r, * c = new Vec[w * h];
+	int w = 1024, 
+		h = 768, 
+		samps = argc == 2 ? atoi(argv[1]) / 4 : 1; // # samples
 
-#pragma omp parallel for schedule(dynamic, 1) private(r)       // OpenMP
+	Ray cam( Vec(50, 52, 295.6), Vec(0, -0.042612, -1).norm() ); // cam pos, dir
+	Vec cx = Vec(w * .5135 / h), 
+		cy = (cx % cam.d).norm() * .5135, 
+		r, 
+		*color = new Vec[w * h];
+
+#pragma omp parallel for schedule(dynamic, 1) private(r)
 	for (int y = 0; y < h; y++) 
 	{// Loop over image rows
 		fprintf(stderr,"\rRendering (%d spp) %5.2f%%",samps*4,100.*y/(h-1));
@@ -166,8 +172,9 @@ int main(int argc, char* argv[])
 		{// Loop cols
 			for (int sy = 0, i = (h - y - 1) * w + x; sy < 2; sy++)     
 			{// 2x2 subpixel rows
-				for (int sx = 0; sx < 2; sx++, r = Vec()) 
+				for (int sx = 0; sx < 2; sx++) 
 				{// 2x2 subpixel cols
+					r = Vec();
 					for (int s = 0; s < samps; s++) 
 					{
 						double r1 = 2 * erand48(Xi), dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
@@ -175,7 +182,7 @@ int main(int argc, char* argv[])
 						Vec d = cx * (((sx + .5 + dx) / 2 + x) / w - .5) + cy * (((sy + .5 + dy) / 2 + y) / h - .5) + cam.d;
 						r = r + radiance(Ray(cam.o + d * 140, d.norm()), 0, Xi) * (1. / samps);
 					} // Camera rays are pushed ^^^^^ forward to start in interior
-					c[i] = c[i] + Vec(clamp(r.x), clamp(r.y), clamp(r.z)) * .25;
+					color[i] = color[i] + Vec(clamp(r.x), clamp(r.y), clamp(r.z)) * .25;
 				}
 			}
 		}
@@ -185,6 +192,6 @@ int main(int argc, char* argv[])
 	fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
 	for (int i = 0; i < w * h; i++)
 	{
-		fprintf(f, "%d %d %d ", toInt(c[i].x), toInt(c[i].y), toInt(c[i].z));
+		fprintf(f, "%d %d %d ", toInt(color[i].x), toInt(color[i].y), toInt(color[i].z));
 	}
 }
